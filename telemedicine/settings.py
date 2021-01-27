10854a8bcb873effc85deb20aa5f61feb6431d86
Michael Ajanaku <remiljw@gmail.com>
2021-01-27 23:35:01 +0100

    - Settings for Browsable APIs

--- telemedicine/settings.py
+++ telemedicine/settings.py
@@ -109,6 +109,8 @@ REST_FRAMEWORK = {
          ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
      'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+     'rest_framework.authentication.BasicAuthentication',
+     'rest_framework.authentication.SessionAuthentication',
      )
 }
 
