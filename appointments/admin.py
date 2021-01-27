76f9e7d81cba92d60a263d361b32ead546267dec
Michael Ajanaku <remiljw@gmail.com>
2021-01-27 23:30:16 +0100

    Registered Models in Admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import User, Patient, Doctor, Appointment, Calendar
+
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(Calendar)
+
+
+admin.site.unregister(Group)
