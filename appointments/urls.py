ecf223e026fc02f958d8897197f721e671b2c787
Michael Ajanaku <remiljw@gmail.com>
2021-01-27 23:34:07 +0100

    - Added url path for Browsable API
    - Added names to url

--- appointments/urls.py
+++ appointments/urls.py
@@ -5,10 +5,9 @@ from . import views
 app_name = 'appointments'
 
 urlpatterns = [
-    path('patient-signup/', views.PatientSignUpView.as_view()),
-    path('signin/', views.UserLoginView.as_view()),
-    path("add-doctor/", views.AddDoctorView.as_view()),
-    # path("pending", views.get_all_pending_leaves),
-    # path("accept/<int:pk>", views.leave_accept),
-    # path("accepted", views.get_all_accepted_leaves),
+    path('patient-signup/', views.PatientSignUpView.as_view(), name='patient-signup'),
+    path('signin/', views.UserLoginView.as_view(), name='signin'),
+    path('add-doctor/', views.AddDoctorView.as_view(), name='add-doctor'),
+    path('doc-calendar/', views.CalendarView.as_view(),  name='doc-calendar'),
+    path('book-appointment/', views.BookAppointmentView.as_view(), name='book-appointment'),
 ]
--- telemedicine/urls.py
+++ telemedicine/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('appointments.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
