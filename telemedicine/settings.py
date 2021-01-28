b5475faf5ea1d21a1bf59562cdad1822bfcf0392
Michael Ajanaku <remiljw@gmail.com>
2021-01-28 23:37:15 +0100

    db

--- telemedicine/settings.py
+++ telemedicine/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
